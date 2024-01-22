9d7e9675eb4e9b1dcc382e01f832b75e1461c684
roland <lukacsiroland01@gmail.com>
2024-01-22 11:48:38 +0100

    remove ships from world when destroyed

--- game/world.go
+++ game/world.go
@@ -11,7 +11,8 @@ func NewWorld(width, height int) *World {
 }
 
 func (world *World) Update() {
-	for _, ship := range world.ships {
+	sh := world.ships
+	for _, ship := range sh {
 		world.check_wall(ship, ship.Front())
 		world.check_wall(ship, ship.Rear())
 		world.check_wall(ship, ship.Left())
@@ -22,6 +23,7 @@ func (world *World) Update() {
 func (world *World) check_wall(ship *Ship, pos *Vector) {
 	if int(pos.X) > world.width || int(pos.Y) > world.height || pos.X < 0 || pos.Y < 0 {
 		ship.destroy()
+		world.Remove(ship)
 	}
 }
 
